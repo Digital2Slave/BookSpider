b8c35f64e4a466c2cd7e9cffb757260e3bf72473
JohnTian <tianzhaixing@126.com>
2015-12-28 16:37:25 +0800

    1004

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ if __name__=='__main__':
     bookcover = db[MONGODB_COLLECTION]
 
     # !< run
-    cnt = 559
-    for isbn in data[559:]:
+    cnt = 1004
+    for isbn in data[1004:]:
         cnt += 1
         print cnt
         asin  = AmazonIsbn2Asin(isbn)
